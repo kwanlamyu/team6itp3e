64b9b5e09635685928c1972fd87377bee86d1661
Kwan Lam Yu <guanlinyu@hotmail.com>
2018-06-06 02:55:05 +0800

    Update Login

--- index.php
+++ index.php
@@ -3,6 +3,27 @@
 
 <?php include 'navigation_logout.php';?>
 
+<form action="login_validation.php" id="login" name="login" method="POST">
+    <div class="form-group">
+        <label for="userid">Username</label>
+        <input type="text" class="form-control" id="userid" name="username" placeholder="Enter you username">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+    </div>
+    
+    <button id="logBtn" type="submit" name="loginbtt"  class="btn btn-log"><span class="fa fa-sign-in fa-fw" aria-hidden="true"></span> Login</button>
+    
+    <div class="form-group">
+        <p><a href="forgotPassword.php">Forgot Password</a></p>
+    </div>
+    
+    <div class="form-group">
+        <p>Don't have an account? <a href="registraition.php"> Sign-up here</a></p>
+    </div>
+    
+</form>
 
 <?php include 'footer.php';?>
 
