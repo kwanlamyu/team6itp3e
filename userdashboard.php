d6f0fe7b1dec5b4d9c65bb87a24b6aded1879cdd
Kwan Lam Yu <guanlinyu@hotmail.com>
2018-06-18 01:58:02 +0800

    Back up

--- userdashboard.php
+++
@@ -1,7 +0,0 @@
-
-<?php include 'header.php';?>
-
-<?php include 'navigation_superadmin.php';?>
-
-
-<?php include 'footer.php';?>
---
+++ userdashboard_old.php
@@ -0,0 +1,38 @@
+<?php include 'header.php';?>
+<?php include 'navigation_superadmin.php';?>
+
+<!--  -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>Serial Number</th>
+					<th>Company Name</th>
+					<th>Header</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>00000000</td>
+					<td>Ah Beng Private Ltd.</td>
+					<td>
+						<button type="button" class="btn btn-danger">Remove</button>
+					</td>
+				</tr>
+			</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<button type="button" class="btn">Export to CSV</button>
+		</div>
+	</div>	
+</div>
+
+<?php include 'footer.php';?>
