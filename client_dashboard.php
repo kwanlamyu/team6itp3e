f33c2af298cd1614631fa51853e0084da63a7458
Jerome <jerome.a.rodrigues@hotmail.com>
2018-06-21 15:39:35 +0800

    update to client-dashboard
    updates to client-dashboard to include buttons for edit and delete accountant account operations

--- client_dashboard.php
+++ client_dashboard.php
@@ -13,5 +13,8 @@
     </h1>
 </div>
 <div>
-    <button id="createAccountant" type="button" onClick="location.href = 'create_accountant.php';" name="createAccountant"  class="btn"><span class="fa fa-sign-in fa-fw" aria-hidden="true"></span> Create Accountant </button>
+    <button id="createAccountant" type="button" onClick="location.href = 'create_accountant.php';" name="createAccountant"  class="btn btn-default"><span class="fa fa-sign-in fa-fw" aria-hidden="true"></span> Create Accountant </button>
+    <button id="editAccountant" type="button" onClick="location.href = 'edit_accountant.php';" name="editAccountant"  class="btn btn-warning"><span class="fa fa-sign-in fa-fw" aria-hidden="true"></span> Create Accountant </button>
+    <button id="deleteAccountant" type="button" onClick="location.href = 'delete_accountant.php';" name="deleteAccountant"  class="btn btn-danger"><span class="fa fa-sign-in fa-fw" aria-hidden="true"></span> Create Accountant </button>
+  
 </div>
