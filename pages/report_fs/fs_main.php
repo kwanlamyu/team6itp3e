1308c70439c208dbd3e809366b89d24ccf90cded
weekokhoe <1602494@SIT.Singaporetech.edu.sg>
2018-07-20 14:38:29 +0800

    fs_main validated

--- pages/report_fs/fs_main.php
+++ pages/report_fs/fs_main.php
@@ -79,7 +79,7 @@ $_SESSION['companyName'] = "Abc Pte. Ltd.";
 									</select>
 									<span><br/>For multiple files: Please upload latest to oldest in sequence</span>
 								</div>
-								<form action="updateCategories.php" method="post" enctype="multipart/form-data" class="m-form m-form--fit m-form--label-align-right">
+								<form name='uploadForm' action="updateCategories.php" method="post" enctype="multipart/form-data" class="m-form m-form--fit m-form--label-align-right" onsubmit="return validateForm()">
 									<div class="col-lg-3">
 										<input class="form-control m-input" type="text" id="clientCompany" name="clientCompany" value="">
 											<span class="m-form__help">
@@ -97,12 +97,12 @@ $_SESSION['companyName'] = "Abc Pte. Ltd.";
 
 											<div class="m-input-icon m-input-icon--left m-input-icon--right">
 
-											<input type="file" class="m-dropzone dropzone m-dropzone--success" name="trialBalances[]" id="m-dropzone-three" accept=".xlsx" >
+											<input type="file" class="m-dropzone dropzone m-dropzone--success" name="trialBalances[]" id="file0" accept=".xlsx" >
 											</div>
-											<label for='yearEnd'>Financial Year Start:</label>
-											<input type='date' class='form-control' id='yearStart' name='yearStart[]' value=''/>
-											<label for='yearEnd'>Financial Year End:</label>
-											<input type='date' class='form-control' id='yearEnd' name='yearEnd[]' value=''/>
+											<label for='yearStart0'>Financial Year Start:</label>
+											<input type='date' class='form-control' id='yearStart0' name='yearStart[]' value=''/>
+											<label for='yearEnd0'>Financial Year End:</label>
+											<input type='date' class='form-control' id='yearEnd0' name='yearEnd[]' value=''/>
 											</div>
 											<div class="m-form__actions">
 												<input type="submit" class="btn btn-accent" value="Upload File" name="submit">
@@ -121,6 +121,79 @@ $_SESSION['companyName'] = "Abc Pte. Ltd.";
     </div>
 
 		<script>
+		function validateForm(){
+			var submitBtn = document.forms['uploadForm']['submit'];
+			submitBtn.disabled = true;
+			var clientCompany = document.forms['uploadForm']['clientCompany'].value;
+			var today = new Date();
+			var numberOfFiles = document.getElementById("tbNumber").value;
+			var dateFlag = 0;
+			var dateError = "";
+			var yearsArray = new Array();
+			var fileFlag = 0;
+			var fileError = "";
+			for (i = 0; i < numberOfFiles; i++){
+				// date check
+				var startDate = document.forms['uploadForm']['yearStart' + i].value;
+				var endDate = document.forms['uploadForm']['yearEnd' + i].value;
+				if(startDate == "" || endDate == "") {
+      		dateFlag = 1;
+					dateError = "Please enter a date";
+					break;
+    		} else {
+					startYear = startDate.substring(0,4);
+					startMonth = startDate.substring(5,7);
+					startDay = startDate.substring(8,10);
+					endYear = endDate.substring(0,4);
+					endMonth = endDate.substring(5,7);
+					endDay = endDate.substring(8,10);
+					var formattedStartDate = new Date(startYear, startMonth - 1, startDay);
+					var formattedEndDate = new Date(endYear, endMonth - 1, endDay);
+					if (formattedStartDate >= today || formattedEndDate >= today){
+						dateFlag = 1;
+						dateError = "Start date should not later than today.";
+						break;
+					} else if (formattedStartDate > formattedEndDate){
+						dateFlag = 1;
+						dateError = "Start date should not be later than end date";
+						break;
+					} else {
+						yearsArray.push(formattedEndDate);
+					}
+				}
+				// check files are not empty
+				var fileInput = document.forms['uploadForm']['file' + i].value;
+				if (fileInput == ""){
+					fileFlag = 1;
+					fileError = "Please select a file to upload";
+					break;
+				}
+			}
+			// check if all files' dates are in descending order
+			for (i = 0; i < yearsArray.length - 1; i++){
+				if (yearsArray[i] < yearsArray[i + 1]){
+					dateFlag = 1;
+					dateError = "Files should be uploading from latest date first";
+					break;
+				}
+			}
+			if (clientCompany == "" || dateFlag == 1 || fileFlag == 1){
+				if (clientCompany == ""){
+					alert("Company Name must be entered");
+				}
+				if (dateFlag == 1){
+					alert(dateError);
+				}
+				if (fileFlag == 1) {
+					alert(fileError);
+				}
+				submitBtn.disabled = false;
+				return false;
+			}	else {
+				return true;
+			}
+		}
+
 		function changeFileUploadForm(){
 			var selectedValue = document.getElementById("tbNumber").value;
 			var currentForm = document.getElementById("formForUploads");
@@ -131,11 +204,11 @@ $_SESSION['companyName'] = "Abc Pte. Ltd.";
 				<span><i class='la la-thumb-tack'></i></span></span>\
 				Select file to upload:\
 				<div class='m-input-icon m-input-icon--left m-input-icon--right'>\
-				<input type='file' class='m-dropzone dropzone m-dropzone--success' name='trialBalances[]' id='m-dropzone-three' accept='.xlsx'>\
-				</div><label for='yearEnd'>Financial Year Start:</label>\
-				<input type='date' class='form-control' id='yearStart' name='yearStart[]' value=''/>\
-				<label for='yearEnd'>Financial Year End:</label>\
-				<input type='date' class='form-control' id='yearEnd' name='yearEnd[]' value=''/></div>";
+				<input type='file' class='m-dropzone dropzone m-dropzone--success' name='trialBalances[]' id='file" + i + "' accept='.xlsx'>\
+				</div><label for='yearStart" + i + "'>Financial Year Start:</label>\
+				<input type='date' class='form-control' id='yearStart" + i + "' name='yearStart[]' value=''/>\
+				<label for='yearEnd" + i + "'>Financial Year End:</label>\
+				<input type='date' class='form-control' id='yearEnd" + i + "' name='yearEnd[]' value=''/></div>";
 			}
 			currentForm.innerHTML += "<div class='m-form__actions'>\
 			<input type='submit' class='btn btn-accent' value='Upload File' name='submit'>\
