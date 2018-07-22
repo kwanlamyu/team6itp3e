1be05263e1f16428cabdbebf35cc4e02bf422cb3
Jerome <jerome.a.rodrigues@hotmail.com>
2018-07-22 17:04:55 +0800

    edit account complete
    form and validation of edit company account complete

--- pages/user_client_admin/edit_work_validation.php
+++ pages/user_client_admin/edit_work_validation.php
@@ -1,8 +1,58 @@
 <?php
+require_once '../db_connection/db.php';
+$edituen = $editedCollaborators = $userID= $roleID= $updateCollaborator="";
+$valid = TRUE; //this var scope ok
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['updateAccountButton'])) {
+        echo "post reg button <br>";
+        
+        if (empty($_POST["uenid"])) {
+            $valid = FALSE;
+        } else{
+            $edituen =($_POST["uenid"]);
+        }
+        
+        if (empty($_POST["edit_Collaborator"])) {
+            $valid = FALSE;
+        } else{
+            $editedCollaborators = implode(', ', $_POST['edit_Collaborator']);
+        }
+                
+        //$userID = $_SESSION["username"];
+        //$roleID = $_SESSION["role_id"];
+        $userID = "Jerome";
+        $roleID = "3";
+        echo "username: ".$userID."<br>";
+        echo "role ID: ".$roleID."<br>";
+        echo gettype($valid).'<br>';
+        
+        if ($valid == TRUE) {
+//            
+            //prepare statement to insert into DB company name and UEN to
+            $updateCollaborator = "UPDATE usermanageaccount SET user_username='" . $editedCollaborators . "' WHERE account_UEN='" . $edituen . "'";
+            $updateSql = $DB_con->prepare($updateCollaborator);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+            echo $updateCollaborator . "<br>";
+            try {
+                $updateSql->execute();
+                
+                echo '<div class="alert alert-success" role="alert">'
+                        . '<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close"></a>'
+                        . ' Account Manager(s) successfully changed'
+                    . '</div>';
+                echo '<span class="text-success"><span class="fa fa-pulse fa-spinner fa-spin fa-fw fa-lg" aria-hidden="true"></span> Redirecting please wait</span>';
+                echo "<meta http-equiv='refresh' content='3;url=../user_client_admin/client_admin_dashboard.php'> ";
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
 
+//            } else {
+//                echo '<div class="alert alert-warning mmbsm" role="alert">Error: ' . $collaboratorsql . '<br>' . $DB_con->error . '</div>';
+//            }
+        }
+
+
+//        header('Location: manage_work_account.php'); 
+    }
+}
+?>
