83897f6ed7b243f8e7480edfa21698cb7015526d
Kwan Lam Yu <guanlinyu@hotmail.com>
2018-07-27 01:26:45 +0800

    mionr update in ui

--- pages/report_fs/fs_index.php
+++ pages/report_fs/fs_index.php
@@ -44,37 +44,9 @@ if (isset($_SESSION['username']) || isset($_SESSION['role_id']) || isset($_SESSI
         <div class="m-subheader ">
             <div class="d-flex align-items-center">
                 <div class="mr-auto">
-                    <h3 class="m-subheader__title m-subheader__title--separator">
+                    <h3 class="m-subheader__title">
                         Financial Statement
                     </h3>
-                    <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-                        <li class="m-nav__item m-nav__item--home">
-                            <a href="#" class="m-nav__link m-nav__link--icon">
-                                <i class="m-nav__link-icon la la-home"></i>
-                            </a>
-                        </li>
-                        <li class="m-nav__separator">
-                            -
-                        </li>
-                        <li class="m-nav__item">
-                            <a href="" class="m-nav__link">
-                                <span class="m-nav__link-text">
-                                    Generate Report
-                                </span>
-                            </a>
-                        </li>
-                        <li class="m-nav__separator">
-                            -
-                        </li>
-                        <li class="m-nav__item">
-                            <a href="" class="m-nav__link">
-                                <span class="m-nav__link-text">
-                                    Financial Statement
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-
                 </div>
             </div>
         </div>
@@ -89,12 +61,14 @@ if (isset($_SESSION['username']) || isset($_SESSION['role_id']) || isset($_SESSI
                                         <i class="la la-gear"></i>
                                     </span>
                                     <h3 class="m-portlet__head-text">
-                                        Select client to work on
+                                        Select Client
                                     </h3>
                                 </div>
                             </div>
                         </div>
                         <div>
+						<div class="m-portlet__body">
+							<div class="m-section">
                           <?php
                           if (count($allUEN) != 0){
                           ?>
@@ -120,16 +94,21 @@ if (isset($_SESSION['username']) || isset($_SESSION['role_id']) || isset($_SESSI
                             <?php
                           } else {
                             ?>
-                            <span>You have not been assigned to a client yet</span>
+							
+                            <span class="m-section__sub">You have not been assigned a client.</span>
+							
                             <?php
                           }
                           ?>
+						  </div>
+							</div>
                           </div>
                       </div>
                   </div>
               </div>
           </div>
       </div>
+	  </div>
       <?php
       if (count($allUEN) != 0){
         ?>
--- pages/user_client_admin/manage_work_account.php
+++ pages/user_client_admin/manage_work_account.php
@@ -11,37 +11,9 @@ if (isset($_SESSION['username']) && $_SESSION['role_id'] == '2') {
         <div class="m-subheader ">
             <div class="d-flex align-items-center">
                 <div class="mr-auto">
-                    <h3 class="m-subheader__title m-subheader__title--separator">
+                    <h3 class="m-subheader__title">
                         Financial Statement
                     </h3>
-                    <!--                    <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-                                            <li class="m-nav__item m-nav__item--home">
-                                                <a href="#" class="m-nav__link m-nav__link--icon">
-                                                    <i class="m-nav__link-icon la la-home"></i>
-                                                </a>
-                                            </li>
-                                            <li class="m-nav__separator">
-                                                -
-                                            </li>
-                                            <li class="m-nav__item">
-                                                <a href="" class="m-nav__link">
-                                                    <span class="m-nav__link-text">
-                                                        Generate Report
-                                                    </span>
-                                                </a>
-                                            </li>
-                                            <li class="m-nav__separator">
-                                                -
-                                            </li>
-                                            <li class="m-nav__item">
-                                                <a href="" class="m-nav__link">
-                                                    <span class="m-nav__link-text">
-                                                        Financial Statement
-                                                    </span>
-                                                </a>
-                                            </li>
-                                        </ul>-->
-
                 </div>
             </div>
         </div>
@@ -58,7 +30,7 @@ if (isset($_SESSION['username']) && $_SESSION['role_id'] == '2') {
                                         <i class="la la-gear"></i>
                                     </span>
                                     <h3 class="m-portlet__head-text">
-                                        Delete Accountant Account
+                                        Tag Accountants
                                     </h3>
                                 </div>
                             </div>
