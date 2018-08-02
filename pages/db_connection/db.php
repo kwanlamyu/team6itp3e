ed28a7c31d3db55d9c2eea5f3a637467a22e3f99
weekokhoe <1602494@SIT.Singaporetech.edu.sg>
2018-08-02 15:14:08 +0800

    Clean up of files and db.php

--- pages/db_connection/db.php
+++ pages/db_connection/db.php
@@ -1,65 +1,4 @@
 <?php
- /*
-  * Jerome local DB
-  */
-// session_start();
-//
-// $DBHOST = 'localhost';
-// $DBNAME = 'ecomplyc_itp';
-// $DBUSER = 'root';
-// $DBPASS = '';
-//
-// try{
-//      $DB_con = new PDO("mysql:host=$DBHOST;port=8889;dbname=$DBNAME",$DBUSER,$DBPASS);
-//      $DB_con ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }
-// catch(PDOException $e){
-//     echo $e->getMessage();
-// }
-
-
- ?>
-
-<?php
-//
-//// Phoebe local DB
-////session_start();
-//
-// $DB_host = "localhost";
-// $DB_user = "root";
-// $DB_pass = "";
-// $DB_name = "kokhoe";
-//
-// try {
-//     $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}", $DB_user, $DB_pass);
-//     $DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     echo $e->getMessage();
-// }
-//?>
-
-<?php
-//
-// Phoebe local DB
-// session_start();
-//
-// $DB_host = "localhost";
-// $DB_user = "root";
-// $DB_pass = "";
-// $DB_name = "ecomplyc_itp";
-//
-// try {
-//    $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}", $DB_user, $DB_pass);
-//    $DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//    echo $e->getMessage();
-// }
-?>
-
-<?php
-/*
- * joey local DB
- */
 session_start();
 
 $DBHOST = 'localhost';
@@ -74,6 +13,4 @@ try{
 catch(PDOException $e){
    echo $e->getMessage();
 }
-
-
 ?>
